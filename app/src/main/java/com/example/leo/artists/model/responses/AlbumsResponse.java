c8c49c4d9d68b169b0e507bacf346ec4b88556a7
leogluck <leogluck@gmail.com>
2017-11-10 02:17:31 +0200

    Removed Unused data from responses

--- app/src/main/java/com/example/leo/artists/model/responses/AlbumsResponse.java
+++ app/src/main/java/com/example/leo/artists/model/responses/AlbumsResponse.java
@@ -22,18 +22,6 @@ public class AlbumsResponse {
         public String getName() { return this.name; }
 
         public void setName(String name) { this.name = name; }
-
-        private String mbid;
-
-        public String getMbid() { return this.mbid; }
-
-        public void setMbid(String mbid) { this.mbid = mbid; }
-
-        private String url;
-
-        public String getUrl() { return this.url; }
-
-        public void setUrl(String url) { this.url = url; }
     }
 
     public class Image {
@@ -64,24 +52,6 @@ public class AlbumsResponse {
 
         public void setPlaycount(int playcount) { this.playcount = playcount; }
 
-        private String mbid;
-
-        public String getMbid() { return this.mbid; }
-
-        public void setMbid(String mbid) { this.mbid = mbid; }
-
-        private String url;
-
-        public String getUrl() { return this.url; }
-
-        public void setUrl(String url) { this.url = url; }
-
-        private Artist artist;
-
-        public Artist getArtist() { return this.artist; }
-
-        public void setArtist(Artist artist) { this.artist = artist; }
-
         private List<Image> image;
 
         public List<Image> getImages() { return this.image; }
@@ -89,51 +59,13 @@ public class AlbumsResponse {
         public void setImage(List<Image> image) { this.image = image; }
     }
 
-    public class Attr {
-        private String artist;
-
-        public String getArtist() { return this.artist; }
-
-        public void setArtist(String artist) { this.artist = artist; }
-
-        private String page;
-
-        public String getPage() { return this.page; }
-
-        public void setPage(String page) { this.page = page; }
-
-        private String perPage;
-
-        public String getPerPage() { return this.perPage; }
-
-        public void setPerPage(String perPage) { this.perPage = perPage; }
-
-        private String totalPages;
-
-        public String getTotalPages() { return this.totalPages; }
-
-        public void setTotalPages(String totalPages) { this.totalPages = totalPages; }
-
-        private String total;
-
-        public String getTotal() { return this.total; }
-
-        public void setTotal(String total) { this.total = total; }
-    }
-
     public class Topalbums {
+
         private List<Album> album;
 
         public List<Album> getAlbum() { return this.album; }
 
         public void setAlbum(List<Album> album) { this.album = album; }
-
-        @SerializedName("@attr")
-        private Attr attr;
-
-        public Attr getAttr() { return this.attr; }
-
-        public void setAttr(Attr attr) { this.attr = attr; }
     }
 
 }
--- app/src/main/java/com/example/leo/artists/model/responses/ArtistResponse.java
+++ app/src/main/java/com/example/leo/artists/model/responses/ArtistResponse.java
@@ -47,62 +47,11 @@ public class ArtistResponse {
 
         public void setListeners(String listeners) { this.listeners = listeners; }
 
-        private String mbid;
-
-        public String getMbid() { return this.mbid; }
-
-        public void setMbid(String mbid) { this.mbid = mbid; }
-
-        private String url;
-
-        public String getUrl() { return this.url; }
-
-        public void setUrl(String url) { this.url = url; }
-
-        private String streamable;
-
-        public String getStreamable() { return this.streamable; }
-
-        public void setStreamable(String streamable) { this.streamable = streamable; }
-
         private List<Image> image;
 
         public List<Image> getImages() { return this.image; }
 
         public void setImage(List<Image> image) { this.image = image; }
-    }
-
-    public class Attr
-    {
-        private String country;
-
-        public String getCountry() { return this.country; }
-
-        public void setCountry(String country) { this.country = country; }
-
-        private String page;
-
-        public String getPage() { return this.page; }
-
-        public void setPage(String page) { this.page = page; }
-
-        private String perPage;
-
-        public String getPerPage() { return this.perPage; }
-
-        public void setPerPage(String perPage) { this.perPage = perPage; }
-
-        private String totalPages;
-
-        public String getTotalPages() { return this.totalPages; }
-
-        public void setTotalPages(String totalPages) { this.totalPages = totalPages; }
-
-        private String total;
-
-        public String getTotal() { return this.total; }
-
-        public void setTotal(String total) { this.total = total; }
     }
 
     public class Topartists
@@ -112,12 +61,5 @@ public class ArtistResponse {
         public List<Artist> getArtist() { return this.artist; }
 
         public void setArtist(List<Artist> artist) { this.artist = artist; }
-
-        @SerializedName("@attr")
-        private Attr attr;
-
-        public Attr getAttr() { return this.attr; }
-
-        public void setAttr(Attr attr) { this.attr = attr; }
     }
 }
